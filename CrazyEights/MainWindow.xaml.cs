d031923f6cac7b4b230bba3abc71744307f5500f
jesusmjc <jesusmanuelmujica12@gmail.com>
2023-10-09 16:30:23 -0600

    Add project files.

---
+++ CrazyEights/MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using ClassLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace CrazyEights
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.WindowStartupLocation = WindowStartupLocation.CenterScreen;
+        }
+
+        /*
+            private void Button_Click(object sender, RoutedEventArgs e)
+            {
+                Jugador tablaJugadores = new Jugador();
+                Usuario tablaUsuarios = new Usuario();
+                {
+                    CrazyEightsEntities CrazyEights = new CrazyEightsEntities();
+                    tablaJugadores.nombreUsuario = tbxNombreUsuario.Text;
+                    tablaUsuarios.contraseña = pwbContrasena.Password;
+
+                    CrazyEights.Jugadors.Add(tablaJugadores);
+                    CrazyEights.Usuarios.Add(tablaUsuarios);
+                    CrazyEights.SaveChanges();
+                }
+            }
+        */
+
+        private void IniciarSesion(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void RecuperarContrasena(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void NavegarARegistroUsuario(object sender, RoutedEventArgs e)
+        {
+            RegistroUsuario registroUsuario = new RegistroUsuario();
+            registroUsuario.WindowStartupLocation= WindowStartupLocation.CenterScreen;
+            registroUsuario.Show();
+            this.Close();
+        }
+    }
+}
---
+++ CrazyEights/RegistroUsuario.xaml.cs
@@ -0,0 +1,65 @@
+﻿using ClassLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CrazyEights
+{
+    /// <summary>
+    /// Interaction logic for RegistroUsuario.xaml
+    /// </summary>
+    public partial class RegistroUsuario : Window
+    {
+        public RegistroUsuario()
+        {
+            InitializeComponent();
+        }
+
+        private void GuardarInformaciónUsuario(object sender, RoutedEventArgs e)
+        {
+            Jugadores tablaJugadores = new Jugadores();
+            Usuarios tablaUsuarios = new Usuarios();
+            {
+                CrazyEightsEntities CrazyEights = new CrazyEightsEntities();
+
+                tablaUsuarios.contraseña = pwbContrasena.Password;
+                tablaUsuarios.correoElectrónico = tbxCorreoElectronico.Text;
+
+                tablaJugadores.nombreUsuario = tbxNombreUsuario.Text;
+
+                CrazyEights.Usuarios.Add(tablaUsuarios);
+                CrazyEights.Jugadores.Add(tablaJugadores);
+                
+                if (CrazyEights.SaveChanges() > 0)
+                {
+                    VentanaConfirmación ventanaConfirmacion = new VentanaConfirmación();
+                    ventanaConfirmacion.WindowStartupLocation = WindowStartupLocation.CenterScreen;
+                    ventanaConfirmacion.Show();
+                }
+            }
+        }
+
+        private void NavegarAIniciarSesión(object sender, RoutedEventArgs e)
+        {
+            MainWindow inicioSesión = new MainWindow();
+            inicioSesión.WindowStartupLocation = WindowStartupLocation.CenterScreen;
+            inicioSesión.Show();
+            this.Close();
+        }
+
+        private void EntrarComoInvitado(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
