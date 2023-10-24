7789973ae14dca6317a624423b14642fe5a5c2bf
jesusmjc <jesusmanuelmujica12@gmail.com>
2023-10-24 00:52:45 -0600

    Caos
    Reestructuración de la solución del servidor. Error al conectar el cliente con el nuevo servicio.

---
+++ CrazyEightsServidor/CrazyEightsServicio/IServicioManejoJugadores.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace CrazyEightsServicio
+ {
+    [ServiceContract]
+    public interface IServicioManejoJugadores
+    {
+        [OperationContract]
+        int GuardarJugador(Usuario usuario, Jugador jugador);
+    }
+
+    [DataContract]
+    public class Usuario
+    {
+        private int _idUsuario;
+        private string _contrasena;
+        private string _correoElectronico;
+        private int _idJugador;
+
+        [DataMember]
+        public int IdUsuario { get { return _idUsuario; } set { _idUsuario = value; } }
+
+        [DataMember]
+        public string Contrasena { get { return _contrasena; } set { _contrasena = value; } }
+
+        [DataMember]
+        public string CorreoElectronico { get { return _correoElectronico; } set { _correoElectronico = value; } }
+
+        [DataMember]
+        public int IdJugador { get { return _idJugador; } set { _idJugador = value; } }
+    }
+
+    [DataContract]
+    public class Jugador
+    {
+        private int _idJugador;
+        private string _nombreUsuario;
+        private int _monedas;
+        private string _fotoPerfil;
+        private int _idUsuario;
+
+        [DataMember]
+        public int IdJugador { get { return _idJugador; } set { _idJugador = value; } }
+
+        [DataMember]
+        public string NombreUsuario { get { return _nombreUsuario; } set { _nombreUsuario = value; } }
+
+        [DataMember]
+        public int Monedas { get { return _monedas; } set { _monedas = value; } }
+
+        [DataMember]
+        public string FotoPerfil { get { return _fotoPerfil; } set { _fotoPerfil = value; } }
+
+        [DataMember]
+        public int IdUsuario { get { return _idUsuario; } set { _idUsuario = value; } }
+    }
+}
---
+++ CrazyEightsServidor/CrazyEightsServicio/ServicioManejoJugadores.cs
@@ -0,0 +1,44 @@
+﻿using DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace CrazyEightsServicio
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "ServicioManejoJugadores" in both code and config file together.
+    public class ServicioManejoJugadores : IServicioManejoJugadores
+    {
+        public int GuardarJugador(Usuario usuario, Jugador jugador)
+        {
+            Jugadores tablaJugadores = new Jugadores();
+            Usuarios tablaUsuarios = new Usuarios();
+            CrazyEightsEntities CrazyEights = new CrazyEightsEntities();
+
+            tablaUsuarios.contraseña = usuario.Contrasena;
+            tablaUsuarios.correoElectrónico = usuario.CorreoElectronico;
+
+            tablaJugadores.nombreUsuario = jugador.NombreUsuario;
+
+            CrazyEights.Usuarios.Add(tablaUsuarios);
+            CrazyEights.Jugadores.Add(tablaJugadores);
+
+            //if (CrazyEights.SaveChanges() > 0)
+            //{
+            //    VentanaConfirmación ventanaConfirmacion = new VentanaConfirmación();
+            //    ventanaConfirmacion.WindowStartupLocation = WindowStartupLocation.CenterScreen;
+            //    ventanaConfirmacion.Show();
+            //}
+            //else
+            //{
+            //    Console.WriteLine("No funciona");
+            //}
+            int numeroCambios = 0;
+            numeroCambios = CrazyEights.SaveChanges();
+
+            return numeroCambios;
+        }
+    }
+}
