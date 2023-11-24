a7f0cf7879f7d9aa554c8aeb23bd845e82f69c68
alvaroLopezM <131178547+alvaroLopezM@users.noreply.github.com>
2023-11-24 12:36:51 -0600

    Prototipo de personalización de perfil

---
+++ Cliente/CrazyEights/JugadorCliente.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrazyEights
+{
+    public class JugadorCliente
+    {
+        //Singletone
+        public static JugadorCliente JugadorDeCliente { get; set; }
+        //
+        public int IdUsuario { get; set; }
+        public int IdJugador { get ; set; }
+        public string NombreUsuario { get; set; }
+        public string FotoPerfil { get; set; }
+        public bool EsInvitado { get; set; }
+    }
+}
