b5eb04c00223121f41826f6adcd83d8e5af56c46
jesusmjc <jesusmanuelmujica12@gmail.com>
2023-12-13 09:58:03 -0600

    wip Sala

---
+++ CrazyEightsServidor/CrazyEightsServicio/IServicioSala.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrazyEightsServicio
+{
+    [ServiceContract]
+    public interface IServicioSala
+    {
+        [OperationContract]
+        int ActualizarConfiguraciónSala(Sala sala);
+    }
+
+    [DataContract]
+    public class Sala
+    {
+        private int _idSala;
+        private int _codigo;
+        private string _nombre;
+        private string _modoDeJuego;
+        private int _numeroDeRondas;
+        private string _tipoDeAcceso;
+
+        [DataMember]
+        public int IdSala { get; set; }
+
+        [DataMember]
+        public int Codigo { get; set; }
+
+        [DataMember]
+        public string Nombre { get; set; }
+
+        [DataMember]
+        public string ModoDeJuego { get; set; }
+
+        [DataMember]
+        public int NumeroDeRondas { get; set; }
+
+        [DataMember]
+        public string TipoDeAcceso { get; set; }
+    }
+}
---
+++ CrazyEightsServidor/CrazyEightsServicio/ServicioSala.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrazyEightsServicio
+{
+    internal class ServicioSala : IServicioSala
+    {
+        public int ActualizarConfiguraciónSala(Sala sala)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
